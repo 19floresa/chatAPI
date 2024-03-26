51817253ad43d395254e05d1871c9eb5481e7de0
19floresa <alexander.flores0920@gmail.com>
2024-03-25 23:19:52 -0700

    simple required urls; not tested

--- routes/routes.js
+++ routes/routes.js
@@ -7,4 +7,45 @@ router.get('/', (req, res) =>
     })
 
 
+router.post('/login', (req, res) =>
+   {
+      try
+      {
+      const { username, password } = req.body
+      if (!username || !password) throw "Error: Missing user info!"
+      
+      /* search db */
+
+      res.status(200).json({code:200, message:"success"}) // maybe send a different html screen?
+      } 
+      catch (e)
+      {
+         res.status(400).json({code:400, message: e})
+      }
+   })
+
+
+router.post('/push', (req, res) =>
+   {
+      try
+      {
+      const { username, message } = req.body
+      if (!username || !message) throw "Error: Missing user info!"
+      
+      /* insert new message to db */
+
+      res.status(200).json({code:200, message:"success"}) // maybe send a different html screen?
+      } 
+      catch (e)
+      {
+         res.status(400).json({code:400, message: e})
+      }
+    })
+
+
+// pop function to remove a message
+
+//update: user gives time they want data from
+
+
 module.exports = router
