54ee4616781dd54268ee321e5812d80284bf1372
19floresa <alexander.flores0920@gmail.com>
2024-04-28 16:25:51 -0700

    made login into a class

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Messages from './Messages';
-import Login from './Login';
+import login_webpage from './Login';
 import Register from './Register';
+
+const Login = (new login_webpage).Login;
 
 class define_webpages {
   constructor(){}
